b006eaca0d144e59bd76ee4b840a4b6b3660798a
Deimokle <q.dutrevis@gmail.com>
2016-07-08 11:16:23 +0200

    Une partie du panier

--- src/EcommerceBundle/Controller/BijouController.php
+++ src/EcommerceBundle/Controller/BijouController.php
@@ -3,11 +3,24 @@
 namespace EcommerceBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use EcommerceBundle\Entity\Produit;
+use EcommerceBundle\form\ProduitType;
+use Symfony\Component\HttpFoundation\Request;
 
 class BijouController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('EcommerceBundle:Default:bijoux.html.twig');
+        $em = $this->getDoctrine()->getManager();
+
+        $produits = $em->getRepository('EcommerceBundle:Produit')->findAll();
+
+        $session = $request->getSession();
+        $panier = $session->get('cartArray');
+
+        return $this->render('EcommerceBundle:Default:bijoux.html.twig', array(
+            'produits'=> $produits,
+            'panieruser' => $panier,
+        ));
     }
 }
