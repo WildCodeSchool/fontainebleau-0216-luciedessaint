674215737528d6741eb2fc150cbecb9693fc7d0b
Deimokle <q.dutrevis@gmail.com>
2016-07-13 10:50:08 +0200

    page detail produit

---
+++ src/EcommerceBundle/Controller/DetailproduitController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace EcommerceBundle\Controller;
+
+use EcommerceBundle\Entity\Commande;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+use EcommerceBundle\Entity\Produit;
+use EcommerceBundle\Form\ProduitType;
+
+class DetailproduitController extends Controller
+{
+    public function indexAction(Request $request, Produit $produit)
+    {
+        $session = $request->getSession();
+        $panieruser = $session->get('cartArray');
+        
+        return $this->render('EcommerceBundle:Default:detailproduit.html.twig', array(
+            'paniers' => $panieruser,
+            'produit' => $produit,
+        ));
+    }
+}
