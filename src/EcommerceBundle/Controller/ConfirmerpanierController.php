52706d053ef55babd6993b8fda25336d7f040636
Deimokle <q.dutrevis@gmail.com>
2016-07-18 09:20:02 +0200

    r

--- src/EcommerceBundle/Controller/ConfirmerpanierController.php
+++ src/EcommerceBundle/Controller/ConfirmerpanierController.php
@@ -4,6 +4,8 @@ namespace EcommerceBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use EcommerceBundle\Entity\Adresse;
+//use EcommerceBundle\Form\AdresseType;
 
 class ConfirmerpanierController extends Controller
 {
@@ -11,9 +13,21 @@ class ConfirmerpanierController extends Controller
     {
         $session = $request->getSession();
         $panieruser = $session->get('cartArray');
-        
+        $total=0;
+        $adresse = new Adresse();
+        $form = $this->createForm('EcommerceBundle\Form\AdresseType', $adresse);
+        $form->handleRequest($request);
+
+        if ($panieruser != null) {
+            foreach ($panieruser as $idx => $article) {
+                $total += $article["prix"];
+            }
+        }
+
         return $this->render('EcommerceBundle:Default:confirmerpanier.html.twig', array(
             'paniers' => $panieruser,
+            'total' => $total,
+            'form' => $form->createView(),
         ));
     }
 }
