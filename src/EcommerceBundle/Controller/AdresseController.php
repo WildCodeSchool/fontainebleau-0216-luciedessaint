c03255ef837d8463116ec9ef8b15594b528d5337
Deimokle <q.dutrevis@gmail.com>
2016-07-28 17:40:36 +0200

    adresse

--- src/EcommerceBundle/Controller/AdresseController.php
+++ src/EcommerceBundle/Controller/AdresseController.php
@@ -67,18 +67,20 @@ class AdresseController extends Controller
             $session->set('adresseArray1', $adresse1);
             $session->set('adresseArray2', $adresse2);
 
+
             //var_dump($adresse_client); die;
             // si il y a une deuxieme adresse alors on persiste $adresse2 sinon $adresse 1 est l'adresse de livraison.
             if ($adresse2->getAdrAdr() != null)
             {
-                $em->persist($adresse2);
+                $session->set('adresseArray2', $adresse2);
+ //               $em->persist($adresse2);
             }
             else {
                 $adresse1->setAdrTypeName('livraison');
             }
-
-            $em->persist($adresse1);
-            $em->flush();
+//
+//            $em->persist($adresse1);
+//            $em->flush();
 
 
             return $this->redirectToRoute('adresse_show');
@@ -99,11 +101,23 @@ class AdresseController extends Controller
 //        $deleteForm = $this->createDeleteForm($adresse);
         $session = $request->getSession();
         $panieruser = $session->get('cartArray');
-        $sessionadresse = $session->get('adresseArray');
+
+        $sessionadresse1 = $session->get('adresseArray1');
+        $sessionadresse2 = $session->get('adresseArray2');
+
+        $total=0;
+
+        if ($panieruser != null) {
+            foreach ($panieruser as $idx => $article) {
+                $total += $article["prix"];
+            }
+        }
 
         return $this->render('EcommerceBundle:adresse:show.html.twig', array(
             'paniers' => $panieruser,
-            'sessionadr' => $sessionadresse,
+            'sessionadr1' => $sessionadresse1,
+            'sessionadr2' => $sessionadresse2,
+            'total' => $total
         ));
     }
 
@@ -111,24 +125,60 @@ class AdresseController extends Controller
      * Displays a form to edit an existing Adresse entity.
      *
      */
-    public function editAction(Request $request, Adresse $adresse)
+    public function editAction(Request $request)
     {
-        $deleteForm = $this->createDeleteForm($adresse);
-        $editForm = $this->createForm('EcommerceBundle\Form\AdresseType', $adresse);
-        $editForm->handleRequest($request);
 
-        if ($editForm->isSubmitted() && $editForm->isValid()) {
+        $session = $request->getSession();
+        $panieruser = $session->get('cartArray');
+
+        $session->get('adresseArray1')->setAdrTypeName('Facturation');
+
+        $adresse_client = new AdresseModele();
+
+        $adresse1 = new AdresseModele();
+        $adresse1->setAdrTypeName('Facturation');
+        $adresse_client->getAdresseModele()->add($adresse1);
+
+        $adresse2 = new AdresseModele();
+        $adresse2->setAdrTypeName('livraison');
+        $adresse_client->getAdresseModele()->add($adresse2);
+
+        $form = $this->createForm(AdresseClientType::class, $adresse_client);
+
+        $form->handleRequest($request);
+        //adresse de livraison n'est pas définit. Et l'est si on click sur plusieur adresse (jquery)
+        // il est définit que si il a plusieurs adresse.
+        $adresse1->setAdrType(1);
+
+        if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
-            $em->persist($adresse);
-            $em->flush();
 
-            return $this->redirectToRoute('adresse_edit', array('id' => $adresse->getId()));
+            $sessionadresse = $session->get('adresseArray1');
+            $sessionadresse = $session->get('adresseArray2');
+            $session->set('adresseArray1', $adresse1);
+
+
+            //var_dump($adresse_client); die;
+            // si il y a une deuxieme adresse alors on persiste $adresse2 sinon $adresse 1 est l'adresse de livraison.
+            if ($adresse2->getAdrAdr() != null)
+            {
+                $session->set('adresseArray2', $adresse2);
+                //               $em->persist($adresse2);
+            }
+            else {
+                $adresse1->setAdrTypeName('livraison');
+            }
+//
+//            $em->persist($adresse1);
+//            $em->flush();
+
+
+            return $this->redirectToRoute('adresse_show');
         }
 
         return $this->render('EcommerceBundle:adresse:edit.html.twig', array(
-            'adresse' => $adresse,
-            'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
+            'paniers' => $panieruser,
+            'form' => $form->createView(),
         ));
     }
 
